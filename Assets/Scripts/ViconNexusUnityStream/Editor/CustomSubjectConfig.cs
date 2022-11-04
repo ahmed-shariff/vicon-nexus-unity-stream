73c335d8904f4a15c2beaa3df651270f2ae24960
Ahmed Shariff <shariff.mfa@outlook.com>
2022-11-04 16:00:22 -0700

    Editor GUI for common settings & useJson toggle

---
+++ Assets/Scripts/ViconNexusUnityStream/Editor/CustomSubjectConfig.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace umanitoba.hcilab.ViconUnityStream
+{
+    [FilePath("UserSettings/Vicon.config", FilePathAttribute.Location.ProjectFolder)]
+    public class CustomSubjectConfig : ScriptableSingleton<CustomSubjectConfig>
+    {
+        public bool enableWriteData = true;
+        public bool useDefaultData = false;
+        public bool useJson = true;
+        public string baseURI = "http://127.0.0.1:5000/marker/test";
+
+        public void Save()
+        {
+            Save(true);
+        }
+    }
+}
---
+++ Assets/Scripts/ViconNexusUnityStream/Editor/CustomSubjectScriptEditor.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace umanitoba.hcilab.ViconUnityStream.Editor
+{
+    [CustomEditor(typeof(CustomSubjectScript), true)]
+    [CanEditMultipleObjects]
+    public class CustomSubjectScriptEditor: UnityEditor.Editor
+    {
+        private static List<CustomSubjectScript> subjectScripts;
+        private CustomSubjectScript customSubjectScript;
+
+        private void OnEnable()
+        {
+            if (subjectScripts == null)
+            {
+                subjectScripts = new List<CustomSubjectScript>();
+                subjectScripts.AddRange(FindObjectsOfType<CustomSubjectScript>());
+            }
+
+            customSubjectScript = target as CustomSubjectScript;
+            if (!subjectScripts.Contains(customSubjectScript))
+            {
+                subjectScripts.Add(customSubjectScript);
+            }
+        }
+
+        private void OnDisable()
+        {
+            subjectScripts = null;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.LabelField("<color=#eeeeee>----- Common Config -----</color>", new GUIStyle() { fontStyle = FontStyle.Bold, alignment = TextAnchor.MiddleCenter, richText = true});
+            EditorGUI.BeginChangeCheck();
+            CustomSubjectConfig.instance.baseURI = EditorGUILayout.TextField("base URI", CustomSubjectConfig.instance.baseURI);
+            CustomSubjectConfig.instance.useDefaultData = EditorGUILayout.Toggle("Use default data", CustomSubjectConfig.instance.useDefaultData);
+            CustomSubjectConfig.instance.useJson = EditorGUILayout.Toggle("Use json for network", CustomSubjectConfig.instance.useJson);
+            CustomSubjectConfig.instance.enableWriteData = EditorGUILayout.Toggle("Write data to file", CustomSubjectConfig.instance.enableWriteData);
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(target);
+                CustomSubjectConfig.instance.Save();
+                foreach (CustomSubjectScript script in subjectScripts)
+                {
+                    script.useDefaultData = CustomSubjectConfig.instance.useDefaultData;
+                    script.useJson = CustomSubjectConfig.instance.useJson;
+                    script.enableWriteData = CustomSubjectConfig.instance.enableWriteData;
+                    script.URI = CustomSubjectConfig.instance.baseURI + script.subjectName;
+                    Debug.Log($"{script.URI}");
+                }
+                Debug.Log($"Setting URI: {customSubjectScript.URI};    Using default data: {customSubjectScript.useDefaultData};     Using json: {customSubjectScript.useJson};    Writing data: {customSubjectScript.enableWriteData};    Scripts updated: \n" + string.Join(",\n", subjectScripts));
+            }
+
+            EditorGUILayout.Space();
+            EditorGUILayout.LabelField("<color=#eeeeee>----- Component Spcific Config -----</color>", new GUIStyle() { fontStyle = FontStyle.Bold, alignment = TextAnchor.MiddleCenter, richText = true});
+            base.DrawDefaultInspector();
+        }
+    }
+}
