adc179d7ce0fb3613ef696708eedabe8677f7a1d
Shariff Faleel <shariff.mfa@outlook.com>
2023-07-20 17:06:26 -0700

    Add oneeurofilter to hwd follower

--- Assets/Scripts/ViconNexusUnityStream/HWDFollower.cs
+++ Assets/Scripts/ViconNexusUnityStream/HWDFollower.cs
@@ -9,6 +9,16 @@ namespace ubc.ok.ovilab.ViconUnityStream
         public Transform base3;
         public Transform base4;
 
+        public bool applyFilter = false;
+        public float filterMinCutoff = 0.1f, filterBeta = 50;
+
+        private OneEuroFilter<Quaternion> filter;
+
+        void Start()
+        {
+            filter = new OneEuroFilter<Quaternion>(90, filterMinCutoff, filterBeta);
+        }
+
         void Update()
         {
             transform.position = base1.position;
@@ -18,7 +28,15 @@ namespace ubc.ok.ovilab.ViconUnityStream
                 Vector3 right = base3.position - base4.position;
                 if (right != Vector3.zero)
                 {
-                    transform.rotation = Quaternion.LookRotation(forward, Vector3.Cross(right, forward));
+                    Quaternion rotation = Quaternion.LookRotation(forward, Vector3.Cross(right, forward));
+                    if (applyFilter)
+                    {
+                        transform.rotation = filter.Filter(rotation, Time.realtimeSinceStartup);
+                    }
+                    else
+                    {
+                        transform.rotation = rotation;
+                    }
                 }
             }
         }
