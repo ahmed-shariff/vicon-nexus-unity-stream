db18dd2c52bf2ed3749f92f324ab2ec785713f86
Shariff Faleel <shariff.mfa@outlook.com>
2023-08-28 10:06:20 -0700

    Better handling data for CustomSubjectConfig

--- Assets/Scripts/ViconNexusUnityStream/Editor/CustomSubjectConfig.cs
+++ Assets/Scripts/ViconNexusUnityStream/Editor/CustomSubjectConfig.cs
@@ -8,11 +8,78 @@ namespace ubc.ok.ovilab.ViconUnityStream
     [FilePath("UserSettings/Vicon.config", FilePathAttribute.Location.ProjectFolder)]
     public class CustomSubjectConfig : ScriptableSingleton<CustomSubjectConfig>
     {
-        public bool enabled = true;
-        public bool enableWriteData = true;
-        public bool useDefaultData = false;
-        public bool useJson = true;
-        public string baseURI = "http://127.0.0.1:5000/marker/test";
+        private bool enabled = true;
+        private bool enableWriteData = true;
+        private bool useDefaultData = false;
+        private bool useJson = true;
+        private string baseURI = "http://127.0.0.1:5000/marker/test";
+
+        public bool Changed { get; private set; } = false;
+
+        public bool Enabled {
+            get {
+                return enabled;
+            }
+            set {
+                if (enabled != value)
+                {
+                    Changed = true;
+                }
+                enabled = value;
+            }
+        }
+
+        public bool EnableWriteData {
+            get {
+                return enableWriteData;
+            }
+            set {
+                if (enableWriteData != value)
+                {
+                    Changed = true;
+                }
+                enableWriteData = value;
+            }
+        }
+
+        public bool UseDefaultData {
+            get {
+                return useDefaultData;
+            }
+            set {
+                if (useDefaultData != value)
+                {
+                    Changed = true;
+                }
+                useDefaultData = value;
+            }
+        }
+
+        public bool UseJson {
+            get {
+                return useJson;
+            }
+            set {
+                if (useJson != value)
+                {
+                    Changed = true;
+                }
+                useJson = value;
+            }
+        }
+
+        public string BaseURI {
+            get {
+                return baseURI;
+            }
+            set {
+                if (baseURI != value)
+                {
+                    Changed = true;
+                }
+                baseURI = value;
+            }
+        }
 
         public void Save()
         {
--- Assets/Scripts/ViconNexusUnityStream/Editor/CustomSubjectScriptEditor.cs
+++ Assets/Scripts/ViconNexusUnityStream/Editor/CustomSubjectScriptEditor.cs
@@ -24,6 +24,15 @@ namespace ubc.ok.ovilab.ViconUnityStream.Editor
             {
                 subjectScripts.Add(customSubjectScript);
             }
+            CustomSubjectConfig.instance.Enabled = customSubjectScript.enabled;
+            CustomSubjectConfig.instance.UseDefaultData = customSubjectScript.useDefaultData;
+            CustomSubjectConfig.instance.UseJson = customSubjectScript.useJson;
+            CustomSubjectConfig.instance.EnableWriteData = customSubjectScript.enableWriteData;
+            CustomSubjectConfig.instance.BaseURI = customSubjectScript.baseURI;
+            if (CustomSubjectConfig.instance.Changed)
+            {
+                UpdateContent();
+            }
         }
 
         private void OnDisable()
@@ -35,37 +44,42 @@ namespace ubc.ok.ovilab.ViconUnityStream.Editor
         {
             EditorGUILayout.LabelField("<color=#eeeeee>----- Common Config -----</color>", new GUIStyle() { fontStyle = FontStyle.Bold, alignment = TextAnchor.MiddleCenter, richText = true});
             EditorGUI.BeginChangeCheck();
-            CustomSubjectConfig.instance.baseURI = EditorGUILayout.TextField("base URI", CustomSubjectConfig.instance.baseURI);
-            CustomSubjectConfig.instance.enabled = EditorGUILayout.Toggle("Enabled", CustomSubjectConfig.instance.enabled);
-            CustomSubjectConfig.instance.useDefaultData = EditorGUILayout.Toggle("Use default data", CustomSubjectConfig.instance.useDefaultData);
-            CustomSubjectConfig.instance.useJson = EditorGUILayout.Toggle("Use json for network", CustomSubjectConfig.instance.useJson);
-            CustomSubjectConfig.instance.enableWriteData = EditorGUILayout.Toggle("Write data to file", CustomSubjectConfig.instance.enableWriteData);
+            CustomSubjectConfig.instance.BaseURI = EditorGUILayout.TextField("base URI", CustomSubjectConfig.instance.BaseURI);
+            CustomSubjectConfig.instance.Enabled = EditorGUILayout.Toggle("Enabled", CustomSubjectConfig.instance.Enabled);
+            CustomSubjectConfig.instance.UseDefaultData = EditorGUILayout.Toggle("Use default data", CustomSubjectConfig.instance.UseDefaultData);
+            CustomSubjectConfig.instance.UseJson = EditorGUILayout.Toggle("Use json for network", CustomSubjectConfig.instance.UseJson);
+            CustomSubjectConfig.instance.EnableWriteData = EditorGUILayout.Toggle("Write data to file", CustomSubjectConfig.instance.EnableWriteData);
             if (EditorGUI.EndChangeCheck())
             {
-                CustomSubjectConfig.instance.Save();
-                foreach (CustomSubjectScript script in subjectScripts)
-                {
-                    script.enabled = CustomSubjectConfig.instance.enabled;
-                    script.useDefaultData = CustomSubjectConfig.instance.useDefaultData;
-                    script.useJson = CustomSubjectConfig.instance.useJson;
-                    script.enableWriteData = CustomSubjectConfig.instance.enableWriteData;
-                    script.baseURI = CustomSubjectConfig.instance.baseURI;
-                    script.UpdateURI();
-                    EditorUtility.SetDirty(script);
-                }
-                Debug.Log($"Updated {subjectScripts.Count} subject script(s):"+
-                          $"\n    Setting URI:        {customSubjectScript.URI};"+
-                          $"\n    Enabled:            {CustomSubjectConfig.instance.enabled};"+
-                          $"\n    Using default data: {customSubjectScript.useDefaultData};"+
-                          $"\n    Using json:         {customSubjectScript.useJson};"+
-                          $"\n    Writing data:       {customSubjectScript.enableWriteData};"+
-                          $"\n    Scripts updated: \n         " +
-                          string.Join(",\n         ", subjectScripts));
+                UpdateContent();
             }
 
             EditorGUILayout.Space();
             EditorGUILayout.LabelField("<color=#eeeeee>----- Component Spcific Config -----</color>", new GUIStyle() { fontStyle = FontStyle.Bold, alignment = TextAnchor.MiddleCenter, richText = true});
             base.DrawDefaultInspector();
+        }
+
+        private void UpdateContent()
+        {
+            CustomSubjectConfig.instance.Save();
+            foreach (CustomSubjectScript script in subjectScripts)
+            {
+                script.enabled = CustomSubjectConfig.instance.Enabled;
+                script.useDefaultData = CustomSubjectConfig.instance.UseDefaultData;
+                script.useJson = CustomSubjectConfig.instance.UseJson;
+                script.enableWriteData = CustomSubjectConfig.instance.EnableWriteData;
+                script.baseURI = CustomSubjectConfig.instance.BaseURI;
+                script.UpdateURI();
+                EditorUtility.SetDirty(script);
+            }
+            Debug.Log($"Updated {subjectScripts.Count} subject script(s):"+
+                      $"\n    Setting URI:        {customSubjectScript.URI};"+
+                      $"\n    Enabled:            {CustomSubjectConfig.instance.Enabled};"+
+                      $"\n    Using default data: {customSubjectScript.useDefaultData};"+
+                      $"\n    Using json:         {customSubjectScript.useJson};"+
+                      $"\n    Writing data:       {customSubjectScript.enableWriteData};"+
+                      $"\n    Scripts updated: \n         " +
+                      string.Join(",\n         ", subjectScripts));
         }
     }
 }
